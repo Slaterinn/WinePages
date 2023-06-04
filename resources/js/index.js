7348c5ed5a238075be122e2b8e3489cb037df9ab
Slaterinn <throsturp@gmail.com>
2023-06-04 21:19:47 +0000

    Reyni að birta gögn

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,3 +1,13 @@
 fetch('./data/wines_json.json')
     .then((response) => response.json())
     .then((json) => console.log(json));
+
+
+function appendData(data) {
+  var mainContainer = document.getElementById("myData");
+  for (var i = 0; i < data.length; i++) {
+    var div = document.createElement("div");
+    div.innerHTML = 'Name: ' + data[i].wine_name + ' ' + data[i].recommendation;
+    mainContainer.appendChild(div);
+  }
+}
