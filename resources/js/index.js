d60aa15069c80418b03ad39590f61c2f10522020
Slaterinn <throsturp@gmail.com>
2023-06-05 21:37:47 +0000

    Update index.js

--- resources/js/index.js
+++ resources/js/index.js
@@ -9,14 +9,28 @@ fetch('./resources/data/wines_json.json')
     console.log(err);
   });
 
+var myEl = document.createElement('div');
 
+var myLi = document.createElement('li');
+
+var myA = document.createElement('a');
+
+var myImg = document.createElement('img');
 
 function appendData(data) {
-  var findContainers = document.getElementsByClassName("media-content");
+  var findContainers = document.getElementsByClassName("cards");
   var mainContainer = findContainers[0]
   for (var i = 0; i < data.length; i++) {
-    var wine_title = document.createElement("p");
-    wine_title.className = "title is-4";
+    var card_container = document.createElement("div");
+    card_container.className = "card container myData";
+    var card_content = document.createElement("div");
+    card_content.className = "card-content"
+    var card_media = document.createElement("div");
+    card_media.className = "media"
+	var card_media_content = document.createElement("div");
+    card_media_content.className = "media-content"
+	var wine_title = document.createElement("div");
+    wine_title.className = "title"
     wine_title.innerHTML = data[i].wine_name;
     mainContainer.appendChild(wine_title);
   }
