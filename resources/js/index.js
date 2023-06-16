21a7def0345248966861e5e8f016a1d6f3fa4a79
Þröstur Pétursson <Throstur.Petursson@landsbankinn.is>
2023-06-16 15:00:06 +0000

    Update index.js

--- resources/js/index.js
+++ resources/js/index.js
@@ -318,6 +318,7 @@ function appendData(data) {
     var footer_link_vin = document.createElement("a");
     footer_link_vin.href = data[i].link;
     footer_link_vin.innerHTML = 'Vínbúðin';
+    footer_link_vin..setAttribute('target', '_blank');
     footer_content_vin.appendChild(footer_link_vin);
 
     var footer_content_viv = document.createElement("p");
@@ -327,6 +328,7 @@ function appendData(data) {
     var footer_link_viv = document.createElement("a");
     footer_link_viv.href = data[i].link_vivino;
     footer_link_viv.innerHTML = 'Vivino';
+    footer_link_viv.setAttribute('target', '_blank');
     footer_content_viv.appendChild(footer_link_viv);
   }
 }
